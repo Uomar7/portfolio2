81681989920cc2330877b087dd6b5844925377de
Uomar <uomarearlie7@gmail.com>
2019-10-29 20:08:30 +0300

    added a contact form.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ window.addEventListener('load', function()
 {
      spinner.style.display = 'none';
 })
+
+const sendEmail = (e) => {
+     e.preventDefault();
+     Email.send({
+          SecureToken : "e3e2e35e-62ca-48c2-9e1c-0cca6408cbf1",
+          To : 'umar.ngare@gmail.com, uomarearlie7@gmail.com',
+          From : $('#fro').val(),
+          Subject : $('#subj').val(),
+          Body : $('#bod').val()
+      }).then(
+        message => alert(`Sent`)
+      );
+}
