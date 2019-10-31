a87e754a1220173b58d4f1a985f64bd0e26f7032
Uomar <uomarearlie7@gmail.com>
2019-10-31 15:01:41 +0300

    Removed the form

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 var spinner = document.getElementById('overlay');
-window.addEventListener('load', function()
-{
+window.addEventListener('load', function () {
      spinner.style.display = 'none';
 })
 
-const sendEmail = (e) => {
-     e.preventDefault();
-     Email.send({
-          SecureToken : "e3e2e35e-62ca-48c2-9e1c-0cca6408cbf1",
-          To : 'umar.ngare@gmail.com, uomarearlie7@gmail.com',
-          From : $('#fro').val(),
-          Subject : $('#subj').val(),
-          Body : $('#bod').val()
-      }).then(
-        message => alert(`Sent`)
-      );
-}
